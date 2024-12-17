e11330e3a5333c93466e7d57c905594e74b264cb
devon <reichardt22@gmail.com>
2024-12-16 18:34:02 -0800

    grammar fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import { Stack } from '@mantine/core'
 import { Router } from './Router'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Stack>
       <Router />

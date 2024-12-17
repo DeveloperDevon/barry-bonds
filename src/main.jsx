1c565e263cd298f8f3760d13cff78bafbcc1de61
devon <reichardt22@gmail.com>
2024-12-16 18:49:43 -0800

    dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import '@mantine/core/styles.css';
-import { createTheme, MantineProvider } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 import { BrowserRouter } from "react-router";
-
-
-const theme = createTheme({})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MantineProvider theme={theme}>
+    <MantineProvider defaultColorScheme='dark'>
       <BrowserRouter>
         <App />
       </BrowserRouter>
